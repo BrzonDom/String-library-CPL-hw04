17ffa647825a66d795c1ea31a7ecb68f752e5453
Dominik Brzoň <brzon.l.dominik@gmail.com>
2023-11-15 14:29:39 +0100

    strcat.c v2.0 - my function

--- strcat.c
+++ strcat.c
@@ -3,6 +3,27 @@
 #include <string.h>
 
 
+char* strcat_f(char* dest, const char* src) {
+
+    unsigned long int i = 0, j = 0;
+
+    while (dest[i] != '\0') i++;
+
+    while (src[j] != '\0') {
+
+        dest[i] = src[j];
+    
+        i++;
+        j++;
+    }
+    
+    dest[i] = '\0';
+
+
+    return dest;
+}
+
+
 int main(void) {
 
     char str1[20] = "Test1";
