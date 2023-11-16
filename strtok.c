ffb1192e0114580319691882598e62be64bf5430
Dominik Brzoň <brzon.l.dominik@gmail.com>
2023-11-16 17:51:53 +0100

    strtok.c v2.3 - my function using my strspn

--- strtok.c
+++ strtok.c
@@ -1,7 +1,53 @@
+#include <corecrt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+    // size_t strspn( const char* dest, const char* src );
+size_t strspn_f(const char* dest, const char* src) {
+
+/*  description:
+        Returns the length of the maximum initial segment (span) of the byte string pointed to by dest,
+        that consists of only the characters found in byte string pointed to by src.
+    */
+
+/*  parametrs:   
+        dest = pointer to the null-terminated byte string to be analyzed
+        src	 = pointer to the null-terminated byte string that contains the characters to search for
+    */
+
+    size_t len = 0, i, j;
+
+    size_t lenDest = 0;
+        while (dest[lenDest] != '\0') lenDest++;
+
+    size_t lenSrc = 0;
+        while (src[lenSrc] != '\0') lenSrc++; 
+    
+    int match = 0;
+
+    for (i = 0; i < lenDest; i++) {
+
+        match = 0;
+
+        for (j = 0; j < lenSrc; j++) {
+            if (dest[i] == src[j]) {
+                match = 1;
+                break;
+            }
+        }
+
+        if (!match) break;
+        else len++;
+
+    }
+
+    return len;
+
+/*  return:
+        The length of the maximum initial segment that contains only characters from byte string pointed to by src.
+    */
+}
 
 static char* iter;
 
@@ -31,7 +77,7 @@ char* strtok_f (char* str, const char* delim) {
     if (str == NULL)
         str = iter;
 
-    str += strspn(str, delim);
+    str += strspn_f(str, delim);
 
     if (*str == '\0') {
         iter = str;
