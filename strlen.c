905c4b92d491943a77c481742518a836681fb628
Dominik Brzoň <brzon.l.dominik@gmail.com>
2023-11-17 12:21:41 +0100

    strlen.c 1.0 library function

---
+++ strlen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(void) {
+
+    char str1[] = "aAbBcCdDeEfFgGhH";
+    char str2[] = "12345678901234567890";
+
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+
+    int siz1 = sizeof(str1);
+    int siz2 = sizeof(str2);
+
+    printf("str1: %s\n", str1);
+    printf("\tlen1: %d\n", len1);
+    printf("\tsiz1: %d\n\n", siz1);
+
+    printf("str2: %s\n", str2);
+    printf("\tlen2: %d\n", len2);
+    printf("\tsiz2: %d\n\n", siz2);
+    
+
+    return 0;
+
+}
